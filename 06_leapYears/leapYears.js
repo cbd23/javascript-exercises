3cf7e838014f856ded00f9f17a03c6ed1dca4252
catalinbadila <112496548+cbd23@users.noreply.github.com>
2023-05-16 14:17:10 +0300

    Add the code needed to pass all the tests.

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,13 @@
-const leapYears = function() {
+const leapYears = function(year) {
 
+// condition 1 - must be divisible by 4
+// condition 2 - if it's a century year, it must also be divisible by 400
+
+   if ((year % 100 == 0) && (year % 400 != 0)) return false;
+   
+   else if (year % 4 != 0) return false;
+
+   else return true;
 };
 
 // Do not edit below this line
