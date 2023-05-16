9484434d81b3c8e51f7fe74191d62334c6a00a7f
catalinbadila <112496548+cbd23@users.noreply.github.com>
2023-05-16 14:36:49 +0300

    Add the code needed to pass all the tests.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,21 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(a) {
+
+  const result = (a - 32) * 5/9;
+
+  const roundedResult = Math.round(result * 10) / 10;
+
+  return roundedResult;
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(b) {
+
+  const result = (b * 1.8) + 32;
+
+  const roundedResult = Math.round(result * 10) / 10;
+
+  return roundedResult;
+
 };
 
 // Do not edit below this line
